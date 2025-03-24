6382dfc2e40b1124062af72aff1c87b6e3d578b4
jazzvestibular <alexandre@jazzvestibular.com.br>
2025-03-24 14:28:08 -0300

    Add files via upload

--- dashboard.py
+++ dashboard.py
@@ -3,7 +3,7 @@ import streamlit as st
 st.set_page_config(page_title="Jazz Vestibular", page_icon="./logo_jazz_menor.png", layout="wide")
 
 #st.set_option('client.caching', False)
-st.cache_data.clear()
+#st.cache_data.clear()
 
 import streamlit.components.v1 as components
 from alunos import mostrar_alunos
